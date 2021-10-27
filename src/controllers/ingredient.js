af823b9d0621e55eee37ee96f50beec320a42891
Guilherme Biage <gbiage@pop-os.localdomain>
2021-10-27 15:26:26 -0300

    added other routes to ingredients controller and finished basic functionality for IngredientService

--- src/controllers/ingredient.js
+++ src/controllers/ingredient.js
@@ -2,11 +2,48 @@ const IngredientService = require('../services/ingredient')
 
 module.exports = app => {
     
-    app.get('/ingredients', (req, res) => {
+    app.get('/ingredient', (req, res) => {
 
         const ingredients = IngredientService.getAll()
+        ingredients.then( (ingredients) => {
+            res.status(201).json(ingredients)
+        })
 
-        res.status(201).json(ingredients)
+    })
+
+    app.get('/ingredient/:id', (req, res) => {
+
+        const ingredient = IngredientService.get(req.params.id)
+        ingredient.then( (ingredient) => {
+            res.status(201).json(ingredient)
+        } )
+
+    })
+
+    app.post('/ingredient', (req,res) => {
+
+        const ingredient = req.body
+        const result = IngredientService.insert(ingredient)
+
+        res.status(201).json(result)
+    })
+
+    app.patch('/ingredient/:id', (req, res) => {
+
+        const id = req.params.id
+        const ingredient = req.body
+        const result = IngredientService.update(id, ingredient)
+
+        res.status(201).json(result)
+    })
+
+    app.delete('/ingredient/:id', (req, res) => {
+
+        const id = req.params.id
+        const result = IngredientService.delete(id)
+
+        res.status(201).json(result)
+
     })
 
 }
--- src/services/ingredient.js
+++ src/services/ingredient.js
@@ -1,22 +1,61 @@
 const Ingredient = require('../models/ingredient')
-const connection = require('../../config/database')
+const connection = require('../../config/database');
 
 class IngredientService{
 
-    getAll() {
-        const ingredients = Ingredient
+    async getAll() {
+        const ingredients = await Ingredient
                                 .find({})
                                 .lean()
                                 .exec()
-                                .then((results) => {
-                                    console.log(results)
-                                    return results
-                                })
-
-        console.log(ingredients)
     
         return ingredients;
     }
+
+    async get(id) {
+        const ingredient = await Ingredient
+                                    .find({ _id: id })
+                                    .lean()
+                                    .exec()
+
+        return ingredient
+    }
+
+    async insert(ingredient) {
+        const result = await Ingredient
+                                .create(ingredient, (e) => {
+                                    console.log(e)
+                                    return
+                                })
+
+        return result
+    }
+
+    async update(id, ingredient) {
+        const result = await Ingredient
+                                .updateOne({ _id: id }, ingredient, (e) => {
+                                    console.log(e)
+                                    return
+                                })
+    }   
+
+    async delete(id) {
+        const result = await Ingredient
+                                .deleteOne({ _id : id }, (e) => {
+                                    console.log(e)
+                                    return
+                                })
+    }
+
+    async getByName(ingredientName) {
+        const ingredients = await Ingredient
+                                    .find({ name: ingredientName})
+                                    .lean()
+                                    .exec()
+
+        return ingredients
+    }
+
 }
 
 module.exports = new IngredientService
