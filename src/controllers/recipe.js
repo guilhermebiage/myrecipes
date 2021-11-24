f127de69c3df3760c4ec7ba4ea35c8cebba9215e
Guilherme Biage <gbiage@pop-os.localdomain>
2021-11-24 10:02:58 -0300

    finished basic functionality for recipe routes on the controller

--- src/controllers/recipe.js
+++ src/controllers/recipe.js
@@ -7,7 +7,16 @@ module.exports = (app) => {
     app.get('/recipe', (req, res) => {
         const recipes = RecipeService.getAll()
         recipes.then((result) => {
-            console.log(result[0].ingredients)
+            res.status(201).json(result)
+        }, (e) => {
+            res.status(400).json(e)
+        })
+    })
+
+    app.get('/recipe/:id', (req,res) => { 
+        const id = req.params.id
+        result = RecipeService.get(id)
+        result.then((result) => {
             res.status(201).json(result)
         }, (e) => {
             res.status(400).json(e)
@@ -17,6 +26,28 @@ module.exports = (app) => {
     app.post('/recipe', (req,res) => {
         const recipe = req.body
         const result = RecipeService.insert(recipe)
+        result.then((result) => {
+            res.status(201).json(result)
+        }, (e) => {
+            res.status(400).json(e)
+        })
+    })
+
+    app.patch('/recipe/:id', (req,res) => {
+        const recipe = req.body
+        const id = req.params.id
+
+        const result = RecipeService.update(id, recipe)
+        result.then((result) => {
+            res.status(201).json(result)
+        }, (e) => {
+            res.status(400).json(e)
+        })
+    })
+
+    app.delete('/recipe/:id', (req, res) => {
+        const id = req.params.id
+        result = RecipeService.delete(id)
         result.then((result) => {
             res.status(201).json(result)
         }, (e) => {
